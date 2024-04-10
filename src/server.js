5f3be13d48b9987c4f172bef8c504f7ce46fa432
kwonsw812 <kwonsw812@gmai.com>
2024-04-10 22:22:34 +0900

    commit before webRTC

--- src/server.js
+++ src/server.js
@@ -19,15 +19,22 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] == "Anon"
     console.log("Connected to Browser");
 
     socket.on("close", () => {
         console.log("Disconnected from the Browser");
     });
 
-    socket.on("message", (message) => {
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
 
-        sockets.forEach(aSocket => aSocket.send(message.toString('utf-8')))
+        switch(message.type) {
+            case 'new_message':
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case 'nickname':
+                socket["nickname"] = message.payload;
+        }
     });
 });
 
