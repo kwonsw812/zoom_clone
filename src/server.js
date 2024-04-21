49e42eed57fabdb88db5e4bcd64adebf2d2c3a11
권상원 <kwon.sangwon@cashwalk.io>
2024-04-21 22:49:02 +0900

    stun server

--- src/server.js
+++ src/server.js
@@ -29,61 +29,80 @@ instrument(wsServer, {
   auth: false,
 });
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
 wsServer.on("connection", (socket) => {
-  socket["nickname"] = "anonymous";
-
-  socket.onAny((event) => {
-    console.log(wsServer.sockets.adapter);
-    console.log(`Socket Event: ${event}`);
-  });
-
-  socket.on("enter_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-
-    wsServer.sockets.emit("room_change", publicRooms());
+    socket.to(roomName).emit("welcome");
   });
 
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
   });
 
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
   });
 
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", (nickname) => {
-    socket["nickname"] = nickname;
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
   });
 });
+
+// function publicRooms() {
+//   const {
+//     sockets: {
+//       adapter: { sids, rooms },
+//     },
+//   } = wsServer;
+//   const publicRooms = [];
+
+//   rooms.forEach((_, key) => {
+//     if (sids.get(key) === undefined) {
+//       publicRooms.push(key);
+//     }
+//   });
+
+//   return publicRooms;
+// }
+
+// function countRoom(roomName) {
+//   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+// }
+
+// wsServer.on("connection", (socket) => {
+//   socket["nickname"] = "anonymous";
+
+//   socket.onAny((event) => {
+//     console.log(wsServer.sockets.adapter);
+//     console.log(`Socket Event: ${event}`);
+//   });
+
+//   socket.on("enter_room", (roomName, done) => {
+//     socket.join(roomName);
+//     done();
+//     socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   });
+
+//   socket.on("disconnecting", () => {
+//     socket.rooms.forEach((room) =>
+//       socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+//     );
+//   });
+
+//   socket.on("disconnect", () => {
+//     wsServer.sockets.emit("room_change", publicRooms());
+//   });
+
+//   socket.on("new_message", (msg, room, done) => {
+//     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+//     done();
+//   });
+
+//   socket.on("nickname", (nickname) => {
+//     socket["nickname"] = nickname;
+//   });
+// });
 
 httpServer.listen(3000, hadleListen);
